aa10f75c30edb35e68938280b67a1fb89b3ccc8c
LoftRain <raven72847771@126.com>
2026-06-18 15:55:43 +0800

    feat(examples): Add comments.

--- examples/json/02-headers/02-headers.js
+++ examples/json/02-headers/02-headers.js
@@ -7,14 +7,16 @@
  *   Read and write complete .level.bin files with sldl-headers.
  */
 
-const { DeclarationGroup, JsonLevelObjects, ItaniumResolver } = require("sldl-jsonify");
+// Step 1: Import sldl-headers.
 const { kLibStdSkyDecl } = require("sldl-headers");
-const { Buffer } = require("buffer");
+const { JsonLevelObjects } = require("sldl-jsonify");
 const fs = require("fs");
 const pl = require("path");
 
 var bin = fs.readFileSync(pl.join(__dirname, "HTNH_TheEnd.Objects.level.bin"));
+// Step 2: Produce the declarations to JsonLevelObjects.
 var objects = new JsonLevelObjects(kLibStdSkyDecl);
+// Step 3: Read or write level objects.
 var jsonify = objects.read(bin);
 
 fs.writeFileSync(pl.join(__dirname, "HTNH_TheEnd.Objects.level.json"), JSON.stringify(jsonify, null, 2));
